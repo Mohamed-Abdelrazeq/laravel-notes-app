8bd38f64e73ba3be4e77eef9fdf08da27c05558f
Mohamed-Abdelrazeq <62867599+Mohamed-Abdelrazeq@users.noreply.github.com>
2023-10-27 05:46:05 +0200

    routing

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+})->name("home");
+
+Route::get('/redirect', function () {
+    return redirect()->route("home");
+});
+
+Route::get('/greet/{name}', function ($name) {
+    return "hello " . $name ."!";
+})->name("greet");
+
+Route::fallback(function () {
+    return "404";
 });
