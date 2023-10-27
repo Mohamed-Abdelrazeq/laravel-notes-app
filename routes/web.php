7a12645d6ddab56bcab80b31ad4ac022484aa3bc
Mohamed-Abdelrazeq <62867599+Mohamed-Abdelrazeq@users.noreply.github.com>
2023-10-27 11:12:26 +0200

    blade template

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index',
+[
+    "name" => "Mohamed"
+]
+);
 })->name("home");
 
 Route::get('/redirect', function () {
